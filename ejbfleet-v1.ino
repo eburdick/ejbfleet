bb5b817d47633d7caa950408b08763e71a1a9f07
Ed Burdick <ed@edburdick.net>
2021-05-20 22:11:32 -0400

    one line comment change to test stage - commit - push from ejbdesk git gui needs a rescan after a file change, then stage change, then commit

--- ejbfleet-v1.ino
+++ ejbfleet-v1.ino
@@ -213,6 +213,7 @@
 
     May 20: no real change. Trying to debug github desk.
     no real change. Testing Git Gui
+    updated ejbdesk repository with fetch followed by merge (pull) now pushing this back
 
 
 */
